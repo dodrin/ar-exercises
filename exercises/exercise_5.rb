0c62dbe3a1da6d358909dfc2e3ff30d822106b11
Rina Inada <indrn@icloud.com>
2023-10-22 05:53:37 +0000

    ex 5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+company_revenue = Store.sum("annual_revenue")
+puts company_revenue
+
+average_revenue = company_revenue / Store.count(:all)
+puts average_revenue
+
+big_stores_count = Store.where('annual_revenue > ?', 1000000).count
+puts big_stores_count
