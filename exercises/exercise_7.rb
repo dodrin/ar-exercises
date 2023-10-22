d443c4d6c6789ec2646e98d4bc75ab97dce71f3f
Rina Inada <indrn@icloud.com>
2023-10-22 06:51:27 +0000

    ex 7 done

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,29 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
+  belongs_to :store
+end
+
+
+class Store
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true}
+end
+
+print "Please enter the store name: "
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Store '#{new_store.name}' has been created with ID: #{new_store.id}"
+else
+  puts "Failed to create the store. Errors:"
+  new_store.errors.full_messages.each do |error_message|
+    puts error_message
+  end
+end
